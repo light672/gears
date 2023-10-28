0c8c0e5744854608380437c9ee52df0eabc16f72
light672 <lightdevelopment6@gmail.com>
2023-10-28 13:17:50 -0400

    Clean unused opcodes.

--- src/main/java/com/light672/roboscript/lang/OpCode.java
+++ src/main/java/com/light672/roboscript/lang/OpCode.java
@@ -2,16 +2,10 @@ package com.light672.roboscript.lang;
 public interface OpCode {
 	byte OP_CONSTANT = 0;
 	byte OP_ADD = 1;
-	byte OP_ADD_NUMBER = 2;
-	byte OP_ADD_STRING = 3;
-	byte OP_SUBTRACT = 4;
-	byte OP_SUBTRACT_NUMBER = 5;
-	byte OP_SUBTRACT_STRING = 6;
-	byte OP_MULTIPLY = 7;
-	byte OP_MULTIPLY_NUMBER = 8;
-	byte OP_DIVIDE = 9;
-	byte OP_DIVIDE_NUMBER = 10;
-	byte OP_RETURN = 11;
-	byte OP_INTERRUPT = 12;
+	byte OP_SUBTRACT = 2;
+	byte OP_MULTIPLY = 3;
+	byte OP_DIVIDE = 4;
+	byte OP_RETURN = 5;
+	byte OP_INTERRUPT = 6;
 
 }
